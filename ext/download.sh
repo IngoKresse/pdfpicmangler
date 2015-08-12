2f94efb6b830041df5bb79bbff9217f486b00bc6
Ingo Kresse <kresse@in.tum.de>
2015-08-12 19:07:19 +0200

    added download script

---
+++ ext/download.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#MIRROR=www.eu.apache.org
+MIRROR=http://mirror2.klaus-uwe.me/apache/
+
+wget $MIRROR/pdfbox/1.8.10/pdfbox-1.8.10.jar
+wget $MIRROR/pdfbox/1.8.10/fontbox-1.8.10.jar
+wget $MIRROR/pdfbox/1.8.10/pdfbox-1.8.10-src.zip
+wget $MIRROR/commons/logging/binaries/commons-logging-1.2-bin.tar.gz
+tar -xzf commons-logging-1.2-bin.tar.gz commons-logging-1.2/commons-logging-1.2.jar
+mv commons-logging-1.2/commons-logging-1.2.jar .
+rmdir commons-logging-1.2/
+
